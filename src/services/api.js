05276c2dea952196ee1407ba4f42db32c52404af
trunght <vuhoang4423@gmail.com>
2023-11-25 21:51:00 +0700

    feature: fix bug keyword explore

--- src/services/api.js
+++ src/services/api.js
@@ -9,26 +9,6 @@ export const login = (data) =>
     data,
   });
 
-export const resetPassword = (data, token) =>
-  publicApi({
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method: 'PUT',
-    url: '/updatePassword',
-    data,
-  });
-
-export const forgotPassword = (email) => {
-  publicApi({
-    method: 'POST',
-    url: '/forgotPassword',
-    data: {
-      email,
-    },
-  });
-};
-
 export const refreshToken = (data) =>
   publicApi({
     method: 'POST',
@@ -42,7 +22,27 @@ export const logout = () =>
     url: '/logout',
   });
 
-  export const getMe = () =>
+export const forgotPassword = (email) => {
+  publicApi({
+    method: 'POST',
+    url: '/forgotPassword',
+    data: {
+      email,
+    },
+  });
+};
+
+export const resetPassword = (data, token) =>
+  publicApi({
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method: 'PUT',
+    url: '/updatePassword',
+    data,
+  });
+
+export const getMe = () =>
   authApi({
     method: 'GET',
     url: '/users/me',
@@ -66,5 +66,242 @@ export const getAmountLoggerByDay = (params) =>
   authApi({
     method: 'GET',
     url: '/loggers/amount-by-day',
+    params,
+  });
+
+// ------------------------------ ExtractVideo ------------------------------
+
+export const getListContent = (params) =>
+  authApi({
+    method: 'GET',
+    url: '/hashtag',
+    params,
+  });
+
+export const getListCampaigns = (params) =>
+  authApi({
+    method: 'GET',
+    url: '/campaigns',
+    params,
+  });
+
+export const getListInputFile = (params) =>
+  authApi({
+    method: 'GET',
+    url: '/input-file',
+    params,
+  });
+
+export const deleteCampaign = (params) =>
+  authApi({
+    method: 'DELETE',
+    url: '/campaigns/' + params?.id,
+    params,
+  });
+
+export const crawlData = (params) =>
+  authApi({
+    method: 'GET',
+    url: '/crawl/' + params.platform,
+    params,
+  });
+
+export const getCampaignContents = ({ campaignId }) =>
+  authApi({
+    method: 'GET',
+    url: `/campaigns/${campaignId}/contents`,
+  });
+
+export const getTiktokPostsByKeyword = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/search/post`,
+    params,
+  });
+
+export const getCategorySuggestion = (params) =>
+  authApi({
+    method: 'POST',
+    url: `suggest`,
+    params,
+  });
+
+export const getInstagramPostsByHashtag = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/instagram/search/posts`,
+    params,
+  });
+
+export const getInstagramNoCORsImage = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/instagram/noCORs`,
+    params,
+  });
+
+export const getPostsByKeywordRapidApi = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/hashtag-posts`,
+    params,
+  });
+
+export const getTiktokHashtagId = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/hashtag-id`,
+    params,
+  });
+
+export const getTiktokUserId = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/user-id`,
+    params,
+  });
+
+export const getTiktokUserInfo = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/user-info`,
+    params,
+  });
+
+export const getTiktokUserPosts = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/user-posts`,
+    params,
+  });
+
+export const getInstagramUserPosts = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/instagram/user-posts`,
+    params,
+  });
+
+export const getInstagramUserInfo = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/instagram/user-info`,
+    params,
+  });
+
+export const getShopeeSearchKeywordVolume = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/shopee/${params.country}/search-keyword-volume`,
+    params,
+  });
+
+export const getGoogleSearchKeywordVolume = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/google/${params.country}/search-keyword-volume`,
+    params,
+  });
+
+export const getLazadaSearchKeywordVolume = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/lazada/${params.country}/search-keyword-volume`,
+    params,
+  });
+
+export const getSearchKeywordSuggestions = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/shopee/${params?.country}/keyword-suggestion`,
+    params,
+  });
+
+export const getListModels = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/suggest/models`,
+  });
+
+export const getModel = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/suggest`,
+    params,
+  });
+
+export const searchModelKeywords = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/suggest/search`,
+    params,
+  });
+
+export const readFileUrl = (params) =>
+  authApi({
+    method: 'GET',
+    url: params.url,
+  });
+
+export const getYoutubeChannelVideos = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/youtube/channel-posts`,
+    params,
+  });
+
+export const getYoutubeSearchVideos = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/youtube/search/posts`,
+    params,
+  });
+
+export const getKeywordsConfidentRate = (params) =>
+  authApi({
+    method: 'POST',
+    url: `/suggest/confidence`,
+    params,
+  });
+
+export const transcriptFromVideoUrl = (params) =>
+  authApi({
+    method: 'POST',
+    url: `/voice-to-text`,
+    params,
+  });
+
+export const getSettingByType = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/settings/type`,
+    params,
+  });
+
+export const getTiktokPostComments = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/post-comments`,
+    params,
+  });
+
+export const getYoutubePostComments = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/youtube/post-comments`,
+    params,
+  });
+
+export const getYoutubeVideoUrl = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/youtube/video-url`,
+    params,
+  });
+
+export const getTiktokPostCommentReplies = (params) =>
+  authApi({
+    method: 'GET',
+    url: `/crawl/tiktok/post-comment-replies`,
     params,
   });
